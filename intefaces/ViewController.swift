0fc79f61891884ce875c9d95c89c97c6913a50dd
Alejandro Salazar <alejandrosalazarcom25@gmail.com>
2024-09-10 17:48:51 -0600

    first commit

---
+++ intefaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  intefaces
+//
+//  Created by imac on 10/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
